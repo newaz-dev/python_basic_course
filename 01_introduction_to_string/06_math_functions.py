cb212e4f49b061df4a75185ebf125a04a21b586b
Md Shah Newaz Fahmir Hridoy <fahmir.hridoy@gmail.com>
2026-09-05 21:34:35 +0600

    06_math_functions.py

---
+++ 01_introduction_to_string/06_math_functions.py
@@ -0,0 +1,12 @@
+# =======Ceil, Floor, and Round=================#
+import math
+x = 10.5
+print(f"X value is: {x}")
+print(f"Floor value is {math.floor(x)}")
+print(f"Ceilling value is {math.ceil(x)}")
+y = 10.45
+z = 10.59
+print(f"Y value is: {y}")
+print(f"rounded value of Y from 10.0 to 10.50 is: {round(y)}")
+print(f"Z value is: {z}")
+print(f"rounded value of Z from 10.51 to 10.99 is: {round(z)}")
