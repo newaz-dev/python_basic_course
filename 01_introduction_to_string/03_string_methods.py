aacb118b3f592ad50efef389c5fba9370531d64f
Md Shah Newaz Fahmir Hridoy <fahmir.hridoy@gmail.com>
2026-09-05 19:13:11 +0600

    methods_of_string

---
+++ 01_introduction_to_string/03_string_methods.py
@@ -0,0 +1,21 @@
+x = "My name is Newaz"
+y0 = print(x.capitalize()) #converts first character into uppercase
+y1 = print(x.casefold()) #converts the hole string into lower case
+y2 = print(x.center(100)) #return a centered string
+y3 = print(x.count(" ")) # count the number of 'a' in that particular sentense
+y4 = print(x.encode())# will return encoded value
+y5 = print(x.find("is"))# It will tell the position of 'is' in that string:x
+a = "this pen is only {price: .2f} dollars"
+y6 = print(a.format(price = 5)) 
+y7 = print(a.index(" ")) 
+y8 = print(a.isdecimal()) 
+y9 = print(a.islower()) 
+y10 = print(a.isupper()) 
+y9 = print(a.lower()) 
+y9 = print(a.upper()) 
+myTuple = ("John", "Peter", "Vicky")
+y10 = print(" ".join(myTuple)) 
+y11 = print(x.replace("Newaz","Fahmir")) #Returns a string where a specified value is replaced with a specified value
+y12 = print(x.title())
+y13 = print(x.swapcase())#capital<==>small
+
