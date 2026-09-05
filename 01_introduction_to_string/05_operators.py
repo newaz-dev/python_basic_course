c28dc1ee4c51a47f091ba70f24a30adb9c68cd39
Md Shah Newaz Fahmir Hridoy <fahmir.hridoy@gmail.com>
2026-09-05 21:11:15 +0600

    operators_precedence

---
+++ 01_introduction_to_string/05_operators.py
@@ -0,0 +1,24 @@
+#     #BODMAS OR                                    PEMDAS RULE
+
+
+# B = Brachet               (or)                   P = Parenthesis
+    
+# O = Order               root  or x**             E = Exponents
+
+# D = Division             / or *                  M = Multiplication  
+
+# M = Multiplication       *  or /                 D = Division
+
+# A = Addition             +                       A = Additiom
+
+# S = Subtraction          -                       S = Subtraction  
+
+# Example Math: 
+x = ( 12 + 34 ) - 45 + 8 / 2 + 12 * 2
+
+# x =      46     - 45 + 8/2   + 12*2
+# x =      46     - 45 +  4    +  24
+# x =      46     - 45 +  4    +  24
+# x =             1    +       28
+# x =                 29: this will be the answer
+print(x)
