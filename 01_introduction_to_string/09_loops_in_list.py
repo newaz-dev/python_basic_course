030a76d5a38f406503304f213392500ad3087bf8
Md Shah Newaz Fahmir Hridoy <fahmir.hridoy@gmail.com>
2026-09-06 00:05:26 +0600

    while_and_for_loop_in_lists

---
+++ 01_introduction_to_string/09_loops_in_list.py
@@ -0,0 +1,12 @@
+# #while loop:
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i= i + 1
+
+
+#For Loop:
+fruit = ["apple", "banana", "kiwi", "mango"]
+for i in fruit:
+    print(i)
