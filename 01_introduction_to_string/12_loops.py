8921d23fc0a2c1f88a68bfc582e69b76bc80403f
Md Shah Newaz Fahmir Hridoy <fahmir.hridoy@gmail.com>
2026-09-06 11:39:24 +0600

    for_loop_fundamentals_with_rangge_list_and_tuple

---
+++ 01_introduction_to_string/12_loops.py
@@ -0,0 +1,14 @@
+# We can implement loops by using range list and tuples
+# Lets say we have a list named a
+a = [1, 2, 3, 4, 5]
+
+# LOOP structure:
+for item in a:
+    print(item)
+# Now a item is present or not will be define by true or False
+print(6 in a) #false
+print(4 in a) #true
+
+for i in range(101):
+    print(f"Nasa is hacking: {i}%")
+print("Nasa is Hacked")
