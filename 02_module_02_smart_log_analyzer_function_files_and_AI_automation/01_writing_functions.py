e678817dcac6ba95facd6499db0e05559f733e01
Md Shah Newaz Fahmir Hridoy <fahmir.hridoy@gmail.com>
2026-09-08 22:56:52 +0600

    functions_types_theory_with_basic_code

---
+++ 02_module_02_smart_log_analyzer_function_files_and_AI_automation/01_writing_functions.py
@@ -0,0 +1,13 @@
+# There are two type of functions 
+# 1. Built-in function, eg: print()
+# 2. User-defined function, eg: this_is_a_function():
+
+# user defined function: 
+def my_first_function():
+    pass
+    print("\nThis is my first user defined type function without parameter passing\n")
+# function calling
+my_first_function()
+
+# ================== Built-in function==============#
+print("This is a built-in function") # Cause it is just builted only for printing....we cannot modify it as we want
