a7efcdd0889d7110c29e48e4e1d5523fb050c432
Md Shah Newaz Fahmir Hridoy <newaz.fahmir@gmail.com>
2026-09-20 14:41:05 +0600

    Abseraction Fundamentals

---
+++ 03_object_oriented_design_and_design patterns/16_abstraction.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):  # Abstract class
+
+    @abstractmethod
+    def start(self):  # Abstract method
+        pass
+
+
+class Car(Vehicle):
+
+    def start(self):
+        print("Car starts using a key")
+
+
+class Bike(Vehicle):
+
+    def start(self):
+        print("Bike starts using a self-starter")
+
+
+car1 = Car()
+bike1 = Bike()
+
+car1.start()
+bike1.start()
