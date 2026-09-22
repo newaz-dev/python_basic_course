2dcdd04ff823a03a17f3722dbdf281e4fb2a2c47
Md Shah Newaz Fahmir Hridoy <newaz.fahmir@gmail.com>
2026-09-23 00:01:17 +0600

    Factory Pattern Fundamentals

---
+++ 03_object_oriented_design_and_design patterns/19_factory_pattern.py
@@ -0,0 +1,13 @@
+class EmailNotification:
+    def send(self):
+        print("Sending Email Notification")
+
+
+class SMSNotification:
+    def send(self):
+        print("Sending SMS Notification")
+
+
+class PushNotification:
+    def send(self):
+        print("Sending Push Notification")
