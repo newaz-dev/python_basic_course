9187dd61d13122e81ee4f31cb4afbe8581d7956c
Md Shah Newaz Fahmir Hridoy <newaz.fahmir@gmail.com>
2026-09-23 00:02:24 +0600

    Factory Pattern Fundamentals

--- 03_object_oriented_design_and_design patterns/19_factory_pattern.py
+++ 03_object_oriented_design_and_design patterns/19_factory_pattern.py
@@ -11,3 +11,31 @@ class SMSNotification:
 class PushNotification:
     def send(self):
         print("Sending Push Notification")
+
+
+class NotificationFactory:
+
+    @staticmethod
+    def create_notification(notification_type):
+
+        if notification_type == "email":
+            return EmailNotification()
+
+        elif notification_type == "sms":
+            return SMSNotification()
+
+        elif notification_type == "push":
+            return PushNotification()
+
+        else:
+            raise ValueError("Invalid notification type")
+
+
+notification1 = NotificationFactory.create_notification("email")
+notification1.send()
+
+notification2 = NotificationFactory.create_notification("sms")
+notification2.send()
+
+notification3 = NotificationFactory.create_notification("push")
+notification3.send()
